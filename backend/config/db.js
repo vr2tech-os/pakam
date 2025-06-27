60589a8e892dddf92d14be6a4bd7ff9097048ef8
vr2tech-os <info@vr2tech.in>
2025-06-27 19:17:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://pakam:12345@cluster0.4kx9hwe.mongodb.net/food-app').then(()=>console.log("DB connected")); 
-}
+  const mongoURI = process.env.MONGODB_URI;
+  await mongoose.connect(mongoURI)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.error("DB connection failed:", err));
+};
